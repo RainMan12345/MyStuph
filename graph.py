416c288d7ace89d10ed3908df0263a27be180a6f
RainMan <aaron.roar@hotmail.com>
2020-08-17 21:54:44 +0000

    added y_axis
    y_axis prints the y_axis onto the screen

--- graph.py
+++ graph.py
@@ -7,4 +7,12 @@ def x_axis(lower_bound, upper_bound):
         i+=1
     return axis
 
-print(x_axis(-3,9))
+def y_axis(lower_bound, upper_bound):
+    domain_length = upper_bound - lower_bound
+    i = 0
+    axis = "|"
+    while (i<70 and i<domain_length-1):
+        axis+="\n|"
+        i+=1
+    return axis
+print(y_axis(1,9) + x_axis(-3,9))
