055459e9328f5e3c2122acb3ab6a8a03aac3b160
RainMan <aaron.roar@hotmail.com>
2020-08-17 21:24:50 +0000

    print x axis
    prints x axis to the screen, depending on the domain size

--- graph.py
+++ graph.py
@@ -1,10 +1,16 @@
-y_axis = """
-|
-|
-|
-|
-|
-"""
-x_axis = "---------------"
+def user_input():
+    lower_bound = input("Enter lower bound: ")
+    upper_bound = input("Enter upper bound: ")
+    return x_axis(int(lower_bound), int(upper_bound))
 
-print(y_axis + x_axis)
+def x_axis(lower_bound, upper_bound):
+    print_amount = upper_bound - lower_bound
+    i = 0
+    dash = "-"
+    axis = ""
+    while (i<70 and i<print_amount):
+        axis+=dash
+        i+=1
+    print(axis)
+
+user_input()
