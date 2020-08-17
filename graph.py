0d8aeb1557086fee272b2bedbe11b3c065b1f788
RainMan <aaron.roar@hotmail.com>
2020-08-17 23:38:23 +0000

    enabled printing on coordinate system in Y-axis
    changed y_axis adding value requirement in function header

--- graph.py
+++ graph.py
@@ -7,12 +7,12 @@ def x_axis(lower_bound, upper_bound):
         i+=1
     return axis
 
-def y_axis(lower_bound, upper_bound):
+def y_axis(lower_bound, upper_bound, number_storage):
     domain_length = upper_bound - lower_bound
     i = 0
     axis = ""
     while (i<70 and i<domain_length):
-        axis+="|\n"
+        axis+="|" + number_storage[(domain_length-1)-i]+"\n"
         i+=1
     return axis
-print(y_axis(1,9) + "+" + x_axis(-3,9))
+print(y_axis(1,4,["","","    "]) + "+" + x_axis(-3,9))
