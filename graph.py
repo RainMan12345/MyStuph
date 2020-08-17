5eea1c9fc18f24864f6aea263e1468e2a74ce92a
RainMan <aaron.roar@hotmail.com>
2020-08-17 22:04:59 +0000

    y_axis variable and print change
    changed how y_axis is printed to screen using <\n> and added <+> to
    print statment

--- graph.py
+++ graph.py
@@ -10,9 +10,9 @@ def x_axis(lower_bound, upper_bound):
 def y_axis(lower_bound, upper_bound):
     domain_length = upper_bound - lower_bound
     i = 0
-    axis = "|"
-    while (i<70 and i<domain_length-1):
-        axis+="\n|"
+    axis = ""
+    while (i<70 and i<domain_length):
+        axis+="|\n"
         i+=1
     return axis
-print(y_axis(1,9) + x_axis(-3,9))
+print(y_axis(1,9) + "+" + x_axis(-3,9))
